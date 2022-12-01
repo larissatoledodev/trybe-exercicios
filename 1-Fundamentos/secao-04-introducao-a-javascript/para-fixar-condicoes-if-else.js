2c5cfcebb568a2b655c3e3cfc2d7b20f07bde72c
larissa <toledobruno610@gmail.com>
2022-12-01 20:57:14 -0300

    criada a estrutura de if/else e impresso o console.log pra nota 58

--- 1-Fundamentos/secao-04-introducao-a-javascript/para-fixar-condicoes-if-else.js
+++ 1-Fundamentos/secao-04-introducao-a-javascript/para-fixar-condicoes-if-else.js
@@ -1,1 +1,9 @@
 const grade = 58;
+
+if (grade >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+} else if (grade < 80 && grade >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else if (grade < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
