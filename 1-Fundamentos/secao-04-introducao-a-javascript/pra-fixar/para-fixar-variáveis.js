6af3ba5630ca0e197b4b863183d636cef016df82
larissa <toledobruno610@gmail.com>
2022-12-01 20:03:15 -0300

    alterado o valor da let birthyear e imprimido o console.log

--- 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar/para-fixar-variáveis.js
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar/para-fixar-variáveis.js
@@ -1,6 +1,10 @@
 const myName = "Larissa";
 const birthCity = "Itabirito";
 let birthYear = 1991;
-console.log(myName);
-console.log(birthCity);
+
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
+
+birthYear = 2030;
 console.log(birthYear);
