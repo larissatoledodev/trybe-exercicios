c93e64ca3c2ccb1161d379f86a8900831642aaec
larissa <toledobruno610@gmail.com>
2022-12-01 21:16:53 -0300

    implementada as condicionais pra imprimir a message referente ao currentHour = 15

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/para-fixar-operadores-lógicos.js
@@ -0,0 +1,16 @@
+const currentHour = 15;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém-passado";
+}
+
+console.log(message);
