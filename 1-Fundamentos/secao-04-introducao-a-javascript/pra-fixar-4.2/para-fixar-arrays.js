7efbdeee9cf14e342d37f22e144ed273520e5d17
larissa <toledobruno610@gmail.com>
2022-12-03 11:10:45 -0300

    realizado o sengo requisito do exercício para fixar arrays

--- 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.2/para-fixar-arrays.js
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.2/para-fixar-arrays.js
@@ -1,5 +1,9 @@
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+
+//console.log(menuServices);
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+let indexOfPortifolio = menu.indexOf('Portfólio');
 
-console.log(menuServices);
-
+console.log(indexOfPortifolio);
