f5d05836eb6966f267038d9b4b1360bf7e12e92f
larissa <toledobruno610@gmail.com>
2022-12-02 22:23:42 -0300

    realizado o exercício 7 da seção 4.1

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercícios/4.1/exercício-7.js
@@ -0,0 +1,15 @@
+const grade = 92;
+
+if (grade >= 90) {
+    console.log("Conceito: A");
+} else if (grade >= 80) {
+    console.log("Conceito: B");
+} else if (grade >= 70) {
+    console.log("Conceito: C");
+} else if (grade >= 60) {
+    console.log("Conceito: D");
+} else if (grade >= 50) {
+    console.log("Conceito: E");
+} else {
+    console.log("Connceito: F");
+} 
