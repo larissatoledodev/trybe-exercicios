7e57fe5538e6e743b2a2a8d2f79472e111d643f3
larissa <toledobruno610@gmail.com>
2022-12-03 20:03:39 -0300

    realizado o requisito 9 do exercício

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.2/exercício-array-requisito-9.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    numbers.push(index);
+}
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2)
+};
