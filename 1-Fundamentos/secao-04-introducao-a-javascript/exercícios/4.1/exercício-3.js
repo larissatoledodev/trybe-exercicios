f69cb4f5d87ca96a7885ac851e028a11e0a24596
larissa <toledobruno610@gmail.com>
2022-12-02 21:29:17 -0300

    realizado o exercício 03

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercícios/4.1/exercício-3.js
@@ -0,0 +1,11 @@
+const a = 7;
+const b = 18;
+const c = 11;
+
+if (a > b && a > c) {
+    console.log("a é o maior número");
+} else if (b > a && b > c) {
+    console.log("b é o maior número");
+} else if (c > a && c > b) {
+    console.log("c é o maior número");
+}
