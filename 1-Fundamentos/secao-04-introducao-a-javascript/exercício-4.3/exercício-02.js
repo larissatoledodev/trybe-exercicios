5bf3ed0102be1dbceb13692be32ee56b445ace33
larissa <toledobruno610@gmail.com>
2022-12-05 18:17:35 -0300

    realizado o exercício 02

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.3/exercício-02.js
@@ -0,0 +1,7 @@
+let word = 'tryber';
+reverseWord = '';
+for (let index = 0; index < word.length; index += 1) {
+    reverseWord += word[word.length - 1 - index];    
+}
+
+console.log(reverseWord);
