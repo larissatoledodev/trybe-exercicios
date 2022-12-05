8282936fbba4dda08ec22e759763c9800eedfa46
larissa <toledobruno610@gmail.com>
2022-12-05 19:05:49 -0300

    realizado o exercício bônus 05

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.3/exercício-bônus-05.js
@@ -0,0 +1,11 @@
+let n = 5;
+let simbol = "*";
+let inputLine = "";
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    inputLine = inputLine + simbol;
+};
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(inputLine);
+};
