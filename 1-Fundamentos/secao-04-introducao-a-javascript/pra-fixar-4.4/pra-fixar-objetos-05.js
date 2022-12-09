3e5501a96fb587afe68d32e5757c679b65918c9b
larissa <toledobruno610@gmail.com>
2022-12-09 11:02:27 -0300

    criado o arquivo pra-fixar-objetos-05.js e resolvido 0 exercício

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.4/pra-fixar-objetos-05.js
@@ -0,0 +1,11 @@
+const player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+};
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
