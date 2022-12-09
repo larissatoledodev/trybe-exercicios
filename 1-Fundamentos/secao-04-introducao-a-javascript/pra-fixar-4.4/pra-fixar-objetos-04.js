82222260a480375d2f2518b6a3cf04850292f226
larissa <toledobruno610@gmail.com>
2022-12-08 22:25:52 -0300

    resolvido para fixar objeto  04

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.4/pra-fixar-objetos-04.js
@@ -0,0 +1,13 @@
+const player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+};
+
+player['bestInTheWord'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWord'].length + ' vezes.');
