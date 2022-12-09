5ad45f0a6ac632693c5e1162d7fa542ed9e60630
larissa <toledobruno610@gmail.com>
2022-12-08 21:28:00 -0300

    resolvido o exercício pra fixar 02 objeto

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.4/pra-fixar-objetos-02.js
@@ -0,0 +1,11 @@
+const player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    }
+};
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
