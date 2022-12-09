dcd601e0f0bfbe75f69b9eecfea71bf4a180b859
larissa <toledobruno610@gmail.com>
2022-12-09 12:08:44 -0300

    resolvido o para fixar funcoes 02

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/pra-fixar-4.4/pra-fixar-funcoes-01.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mult(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+function mod(a, b) {
+    return a % b;
+}
+
