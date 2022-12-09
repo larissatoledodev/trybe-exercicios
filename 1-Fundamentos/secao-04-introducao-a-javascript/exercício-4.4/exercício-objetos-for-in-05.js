9d6087ee946369dc502d10a5250e837cd69998d0
larissa <toledobruno610@gmail.com>
2022-12-09 13:48:35 -0300

    resolvido todos os requisitos do exercício objeto e for/in 05

--- 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.4/exercício-objetos-for-in-05.js
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.4/exercício-objetos-for-in-05.js
@@ -4,7 +4,22 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
   };
-
-  for(let keys in info) {
-    console.log(info[keys]);
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  
+  for (let properties in info) {
+    if (
+      properties === 'recorrente' &&
+      info[properties] === 'Sim' &&
+      info2[properties] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[properties] + ' e ' + info2[properties]);
+    }
   }
