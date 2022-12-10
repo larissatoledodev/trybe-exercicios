a380eaa1a0e6999e7b2d6d07ef45ea00e326706c
larissa <toledobruno610@gmail.com>
2022-12-09 21:03:55 -0300

    resolvido exercício parte.1 04

---
+++ 1-Fundamentos/secao-04-introducao-a-javascript/exercício-4.5/exercício-parte.1-04.js
@@ -0,0 +1,10 @@
+const nome = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá, ${nome} ${lastName}!`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
